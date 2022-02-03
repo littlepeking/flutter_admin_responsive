5603edcc9f1e9650941115d4f2f1ef91b0ef4ed8
John Wang <john.wang_ca@hotmail.com>
2022-02-03 15:39:23 -0500

    nested tabs

---
+++ lib/main/components/home/components/dashboard/components/wmsPanel/components/receipt/receipt_edit_controller.dart
@@ -0,0 +1,8 @@
+import 'package:eh_flutter_framework/main/common/base/EHController.dart';
+import 'package:eh_flutter_framework/main/common/widgets/eh_tabs_view/eh_tabs_view_controller.dart';
+
+class ReceiptEditController extends EHController {
+  EHTabsViewController receiptHeaderTabsViewController = EHTabsViewController();
+
+  EHTabsViewController receiptDetailTabsViewController = EHTabsViewController();
+}
