48b95e9c0289210c316e70283ae35225d1c02f72
John Wang <john.wang_ca@hotmail.com>
2022-02-12 23:41:49 -0500

    focusNode must in controller...

--- lib/main/components/home/components/dashboard/components/wmsPanel/components/receipt/receipt_detail_view_controller.dart
+++ lib/main/components/home/components/dashboard/components/wmsPanel/components/receipt/receipt_detail_view_controller.dart
@@ -1,9 +1,13 @@
 import 'package:eh_flutter_framework/main/common/base/EHEditPanelController.dart';
 import 'package:eh_flutter_framework/main/components/home/components/dashboard/components/wmsPanel/components/receipt/models/receipt_model.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get_rx/src/rx_types/rx_types.dart';
 
 class ReceiptDetailViewController extends EHEditPanelController {
   Rx<ReceiptModel> receiptModel = ReceiptModel(
           receiptKey: 'key001', customerName: 'cus001', dropdownValue: '')
       .obs;
+
+  FocusNode fnButton = FocusNode();
+  FocusNode fnText1 = FocusNode();
 }
