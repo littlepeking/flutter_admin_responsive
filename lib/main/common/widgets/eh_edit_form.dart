3e5fbc77b0cdeae43b77db3a423f45be2f111f93
John Wang <john.wang_ca@hotmail.com>
2022-03-03 13:35:59 -0500

    bug fix

--- lib/main/common/widgets/eh_edit_form.dart
+++ lib/main/common/widgets/eh_edit_form.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:eh_flutter_framework/main/common/base/EHController.dart';
 import 'package:eh_flutter_framework/main/common/base/EHEditWidgetController.dart';
 import 'package:eh_flutter_framework/main/common/base/EHEditableWidget.dart';
